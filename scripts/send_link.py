0d7593d0a8690e8700f52d936a7790e4fdef7d77
Vasiliy Shapovalov <vs@p2p.org>
2021-01-11 18:20:32 +0300

    unlock account

--- scripts/send_link.py
+++ scripts/send_link.py
@@ -6,6 +6,7 @@ link = interface.ERC20("0x514910771af9ca656af840dff83e8264ecf986ca")
 
 def main():
     account = '0x23c8Fbd5E14A9565707D8D1a88045F2fA5648968'
+    accounts.load(account)
     link_balance = link.balanceOf(account)
     print(f'link balance {link_balance}')
     link.transfer('0xB45A43e998286ab3Be4106b4c381f01dccE772a4', 1, {'from': account}) #test
