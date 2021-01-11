5c7650add2ed7836db721e1e84355b4eae5f96f6
Vasiliy Shapovalov <vs@p2p.org>
2021-01-11 19:20:59 +0300

    unlock account

--- scripts/send_link.py
+++ scripts/send_link.py
@@ -5,6 +5,7 @@ link = interface.ERC20("0x514910771af9ca656af840dff83e8264ecf986ca")
 
 
 def main():
+    accounts.clear()
     account = '0x23c8Fbd5E14A9565707D8D1a88045F2fA5648968'
     from_acc =  accounts.load('chainlink-nya3')
     link_balance = link.balanceOf(account)
